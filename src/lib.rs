9ba3e74c348f305365cfd09686ffae1f06138f58
Anders <git@anders.place>
2024-06-12 17:52:01 -0500

    Trying this

--- src/lib.rs
+++ src/lib.rs
@@ -371,14 +371,46 @@ pub fn jsonrpc_message_derive(input: TokenStream) -> TokenStream {
     let mut id_type = None;
     let mut has_id = false;
 
+    // match input.data {
+    //     Data::Struct(ref data) => {
+    //         if let Fields::Named(fields) = data.fields {
+    //             for field in fields.named {
+    //                 if let Some(ident) = field.ident {
+    //                     if ident == "id" {
+    //                         id_field = Some(ident);
+    //                         id_type = Some(field.ty);
+    //                         has_id = true;
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     Data::Enum(ref data) => {
+    //         for variant in data.variants {
+    //             if let Fields::Named(fields) = variant.fields {
+    //                 for field in fields.named {
+    //                     if let Some(ident) = field.ident {
+    //                         if ident == "id" {
+    //                             id_field = Some(ident);
+    //                             id_type = Some(field.ty);
+    //                             has_id = true;
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     _ => panic!("JsonRpcMessage can only be derived for structs and enums"),
+    // }
+
     match input.data {
         Data::Struct(ref data) => {
-            if let Fields::Named(fields) = data.fields {
-                for field in fields.named {
+            if let Fields::Named(ref fields) = data.fields {
+                for field in fields.named.iter() {
                     if let Some(ident) = field.ident {
                         if ident == "id" {
                             id_field = Some(ident);
-                            id_type = Some(field.ty);
+                            id_type = Some(field.ty.clone());
                             has_id = true;
                         }
                     }
@@ -386,13 +418,13 @@ pub fn jsonrpc_message_derive(input: TokenStream) -> TokenStream {
             }
         }
         Data::Enum(ref data) => {
-            for variant in data.variants {
-                if let Fields::Named(fields) = variant.fields {
-                    for field in fields.named {
+            for variant in data.variants.iter() {
+                if let Fields::Named(ref fields) = variant.fields {
+                    for field in fields.named.iter() {
                         if let Some(ident) = field.ident {
                             if ident == "id" {
                                 id_field = Some(ident);
-                                id_type = Some(field.ty);
+                                id_type = Some(field.ty.clone());
                                 has_id = true;
                             }
                         }
